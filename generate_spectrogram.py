566c6695db580fef430d9a47abb41d6540adb74c
Igor Bispo <igorbispo@localhost.localdomain>
2018-11-25 11:18:57 -0200

    Adicionado pesos cnn

---
+++ generate_spectrogram.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import glob
+
+from scipy.io import wavfile
+from scipy.signal import spectrogram
+
+AUDIO_FOLDERS = [
+    'backward', 'bed', 'bird', 'cat', 'dog', 'down',
+    'eight', 'five', 'follow', 'forward', 'four', 'go',
+    'happy', 'house', 'learn', 'left', 'marvin', 'nine',
+    'no', 'off', 'on', 'one', 'right', 'seven', 'sheila',
+    'six', 'stop', 'three', 'two', 'up', 'visual', 'wow',
+    'yes', 'zero'
+]
+
+def generate_spec(filename):
+    sr, data = wavfile.read(filename)
+    fig,ax = plt.subplots(1)
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    ax.axis('off')
+    pxx, freqs, bins, im = ax.specgram(x=data, Fs=sr, noverlap=384, NFFT=512, cmap='gray')
+    ax.axis('off')
+    fig.canvas.draw()
+    X = np.array(fig.canvas.renderer._renderer)[..., 0]
+    plt.close()
+
+    return X.reshape(X.shape[0], X.shape[1], 1)
+
+def generate_all():
+    for folder in AUDIO_FOLDERS:
+        print("\nCurrently generating spectrograms for ", folder)
+        curr_spect = []
+        i = 0
+        for f in glob.glob(folder + "/*.wav"):
+            print("\r{}".format(i), end="")
+            curr_spect.append(generate_spec(f))
+            i += 1
+        curr_spect = np.array(curr_spect)
+        np.save("specs_vectors/" + folder + ".npy", curr_spect)
+            
+
+
+if __name__ == '__main__':
+    generate_all()
+
---
+++ predict_live.py
@@ -0,0 +1,64 @@
+import sounddevice as sd
+import numpy as np
+import time 
+import os
+import generate_spectrogram as gs
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+os.environ["KERAS_BACKEND"] = "plaidml.keras.backend" 
+
+from keras.models import load_model
+
+sd.default.samplerate = 16000
+sd.default.channels = 1
+fs = 16000
+
+def process_spec(s):
+    spect = s
+    spect = cv.resize(spect, (96 , 96)).reshape((96, 96, 1)) 
+    spect = np.stack((spect, spect, spect), axis=2).reshape((1, 96, 96, 3)) 
+    spect = spect * 1/255 
+    
+    return spect
+
+def gen_spect(data):
+    sr = fs
+    data = data.reshape(16000)
+    fig,ax = plt.subplots(1)
+    fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+    ax.axis('off')
+    pxx, freqs, bins, im = ax.specgram(x=data, Fs=sr, noverlap=384, NFFT=512, cmap='gray')
+    ax.axis('off')
+    fig.canvas.draw()
+    X = np.array(fig.canvas.renderer._renderer)[..., 0]
+    plt.close()
+    return X.reshape(X.shape[0], X.shape[1], 1)
+
+def listen():
+    model = load_model("cnn_audio.h5")
+    name_window = "Spectrogram"
+    
+    while(1):
+        r = sd.rec(int(fs))
+        sd.wait()
+        r_spect = gen_spect(r)
+        processed_r = process_spec(r_spect)
+
+        cv.imshow(name_window, r_spect.reshape((480, 640)))
+
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+        p = model.predict(processed_r)
+        label = gs.AUDIO_FOLDERS[p.argmax()]
+
+        print("Word labed as {}".format(label))
+
+if __name__ == "__main__":
+    listen()
+
+
+
+
+
